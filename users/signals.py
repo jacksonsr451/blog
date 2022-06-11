30531ae8f319d59a5adcf5b8205b06127891b9a5
jacksonsr451 <jackson.severino.rocha@escola.pr.gov.br>
2022-06-11 16:40:09 -0300

    Refactoring: update signals

--- users/signals.py
+++ users/signals.py
@@ -6,27 +6,28 @@ from rolepermissions.roles import assign_role, clear_roles
 
 @receiver(post_save, sender=Users)
 def assign_user_role(sender, instance, created, **kwargs):
-    # TODO: Use the created flag to check if the user is new or not
+    if_created_instance(sender, instance, created, **kwargs)
+    if_update_instance(sender, instance, created, **kwargs)
+
+def if_created_instance(sender, instance, created, **kwargs):
     if created:
-        if instance.occupation == 'editor':
-            assign_role(instance, 'editor')
-        elif instance.occupation == 'admin':
-            assign_role(instance, 'admin')
-        elif instance.occupation == 'user':
-            assign_role(instance, 'user')
-    elif not created:
-        if instance.occupation == 'guest':
-            clear_roles(instance)
-            assign_role(instance, 'guest')
-        elif instance.occupation == 'banned':
-            clear_roles(instance)
-            assign_role(instance, 'banned')
-        elif instance.occupation == 'editor':
-            clear_roles(instance)
-            assign_role(instance, 'editor')
-        elif instance.occupation == 'admin':
-            clear_roles(instance)
-            assign_role(instance, 'admin')
-        elif instance.occupation == 'user':
-            clear_roles(instance)
-            assign_role(instance, 'user')
+        create_role_instance(instance, 'editor', 'editor')
+        create_role_instance(instance, 'admin', 'admin')
+        create_role_instance(instance, 'user', 'user')
+
+def if_update_instance(sender, instance, created, **kwargs):
+    if not created:
+        update_role_instance(instance, 'guest', 'guest')
+        update_role_instance(instance, 'banned', 'banned')
+        update_role_instance(instance, 'editor', 'editor')
+        update_role_instance(instance, 'admin', 'admin')
+        update_role_instance(instance, 'user', 'user')
+
+def create_role_instance(instance, role, method):
+    if instance.occupation == role:
+        assign_role(instance, method)
+
+def update_role_instance(instance, role, method):
+    if instance.occupation == role:
+        clear_roles(instance)
+        assign_role(instance, method)
