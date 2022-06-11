728f72a01274c9cc4b36f2f71261c010b8f95930
jacksonsr451 <jackson.severino.rocha@escola.pr.gov.br>
2022-06-10 22:32:25 -0300

    Refactoring: update routers name

--- users/urls.py
+++ users/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('editor/', views.home_editor, name='home_editor'),
-    path('add_editor/', views.add_editor, name='add_editor'),
-    path('insert_editor/', views.insert_editor, name='insert_editor'),
-    path('list_editor/', views.list_editor, name='list_editor'),
-    path('change_editor/', views.change_editor, name='change_editor'),
-    path('delete_editor/', views.delete_editor, name='delete_editor'),
-    path('invite_editor/', views.invite_editor, name='invite_editor'),
-    path('update_editor/', views.update_editor, name='update_editor'),
-    path('ban_editor/', views.ban_editor, name='ban_editor'),
+    path('editors/', views.home_editor, name='home_editor'),
+    path('editors/add/', views.add_editor, name='add_editor'),
+    path('editors/insert/', views.insert_editor, name='insert_editor'),
+    path('editors/list/', views.list_editor, name='list_editor'),
+    path('editors/change/', views.change_editor, name='change_editor'),
+    path('editors/delete/', views.delete_editor, name='delete_editor'),
+    path('editors/invite/', views.invite_editor, name='invite_editor'),
+    path('editors/update/', views.update_editor, name='update_editor'),
+    path('editors/ban/', views.ban_editor, name='ban_editor'),
 ]
