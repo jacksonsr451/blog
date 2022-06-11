5048ac06bda3c2245819ba128ed977c927fe63d0
jacksonsr451 <jackson.severino.rocha@escola.pr.gov.br>
2022-06-10 23:30:41 -0300

    Refactoring: add static files

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -8,4 +10,4 @@ urlpatterns = [
     path('admin_area/', admin.site.urls),
     path('' , views.home, name='home'),
     path('admin/', include('admin_area.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ static/js/change_editors.js
@@ -0,0 +1,24 @@
+function delete_editor(route) {
+    // TODO: add a function to request permission to delete user in template
+    let form = document.getElementById('change_editor_form');
+    form.action = route
+    form.submit();
+}
+
+function update_editor(route) {
+    let form = document.getElementById('change_editor_form');
+    form.action = route
+    form.submit();
+}
+
+function invite_editor(route) {
+    let form = document.getElementById('change_editor_form');
+    form.action = route
+    form.submit();
+}
+
+function ban_editor(route) {
+    let form = document.getElementById('change_editor_form');
+    form.action = route
+    form.submit();
+}
